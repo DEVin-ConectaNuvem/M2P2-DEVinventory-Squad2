6b982ef39339c3e3a7050227d466ecb3a9655c9a
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-08-16 22:58:07 -0300

    feature/adicionado click command populate_db

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
+import click
+from flask.cli import with_appcontext
 from src.app import create_app
+from src.app.database import populate_db
 
 
 app = create_app()
+
+
+@click.command(name='populate_db')
+@with_appcontext
+def call_command():
+    populate_db()
+
+
+app.cli.add_command(call_command)
 
 
 if __name__ == "__main__":
