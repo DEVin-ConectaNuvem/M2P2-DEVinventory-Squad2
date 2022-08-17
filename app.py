236771e6fc08a881074995fd8659ad93aa0a6004
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-08-17 01:23:12 -0300

    feature/adicionado o click command para deletar tabelas do db

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import click
 from flask.cli import with_appcontext
-from src.app import create_app
+from src.app import create_app, db
 from src.app.database import populate_db
 
 
@@ -12,8 +12,13 @@ app = create_app()
 def call_command():
     populate_db()
 
+@click.command(name='drop_all_tables') # a tabela alembic_version continuará existindo
+@with_appcontext
+def drop_all_tables():
+    db.drop_all()
 
 app.cli.add_command(call_command)
+app.cli.add_command(drop_all_tables)
 
 
 if __name__ == "__main__":
