0103af405b2de6bef5c3692c3e710d164ce41634
Breno <hentgenp@gmail.com>
2022-08-18 15:38:56 -0300

    feature/controllers-users finalizado codigo de listagem

--- src/app/controllers/users.py
+++ src/app/controllers/users.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, jsonify, request
 
-from src.app.models.user import User, users_share_schema
+from src.app.models.user import User, users_roles_share_schema
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
 @user.route("/", methods = ['GET'])
 def list_user():
     
-    list_users = User.query.all()
+    list_users = User.query.paginate(per_page=20, error_out=True)
 
-    list_users_dict = users_share_schema.dump(list_users)
+    list_users_dict = users_roles_share_schema.dump(list_users.items)
 
     return jsonify(list_users_dict), 200
 
@@ -20,11 +20,9 @@ def list_user_by_name(name):
 
     list_name_user = User.query.filter(User.name.like(f"%{name_cap}%")).all()
 
-    list_name_dict = users_share_schema.dump(list_name_user)
+    list_name_dict = users_roles_share_schema.dump(list_name_user)
 
     if list_name_dict == []:
         return jsonify({"message": "Usuário não encontrado"}), 204
 
     return jsonify(list_name_dict), 200
-
-
