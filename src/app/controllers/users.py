bf58c93c7af031a3b8f821c2c61b040147d0f91a
Breno <hentgenp@gmail.com>
2022-08-18 18:44:26 -0300

    refactor/refeito o código da endpoint 5

--- src/app/controllers/users.py
+++ src/app/controllers/users.py
@@ -4,25 +4,30 @@ from src.app.models.user import User, users_roles_share_schema
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
-@user.route("/", methods = ['GET'])
-def list_user():
+@user.route("/", defaults = {"users": 1})
+@user.route("/<int:users>", methods = ['GET'])
+@user.route("/<string:users>", methods = ['GET'])
+def list_user_per_page(users):
     
-    list_users = User.query.paginate(per_page=20, error_out=True)
+    if type(users) == str:
+
+        list_name_user = User.query.filter(User.name.ilike(f"%{users}%")).all()
+
+        list_name_dict = users_roles_share_schema.dump(list_name_user)
+
+        if list_name_dict == []:
+
+            error = {
+                "Error": "Usuário não encontrado."
+            }
+
+            return jsonify(error), 204
+
+        return jsonify(list_name_dict), 200
+
+    list_users = User.query.paginate(per_page=2, page=users, error_out=True)
 
     list_users_dict = users_roles_share_schema.dump(list_users.items)
 
     return jsonify(list_users_dict), 200
 
-@user.route("/<string:name>", methods = ['GET'])
-def list_user_by_name(name):
-
-    name_cap = name.capitalize()
-
-    list_name_user = User.query.filter(User.name.like(f"%{name_cap}%")).all()
-
-    list_name_dict = users_roles_share_schema.dump(list_name_user)
-
-    if list_name_dict == []:
-        return jsonify({"message": "Usuário não encontrado"}), 204
-
-    return jsonify(list_name_dict), 200
