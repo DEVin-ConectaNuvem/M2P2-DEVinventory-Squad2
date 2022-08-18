004402be8993988df65e5d661e14f8733c5604b0
Breno <hentgenp@gmail.com>
2022-08-17 21:00:24 -0300

    feature/update app.py / controllers/users.py app/__init__.py

--- src/app/controllers/users.py
+++ src/app/controllers/users.py
@@ -1,7 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+
+from src.app.models.user import User, users_share_schema
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
-@user.route("/", methods = ['GET'])
+@user.route("/", methods = ['POST'])
 def list_user():
-    return "<p>Lista de usuários!</p>"
+    
+    data = request.get_json()
+
+    list_users = User.query.all()
+
+    list_users_dict = users_share_schema.dump(list_users)
+
+    if data['name'] == "":
+        
+        return jsonify(list_users_dict), 200
+
+
+    return jsonify(data), 200
+
+
