4b32d70e14945762ba00dc34b67a0f7a31e02c5b
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-08-18 16:12:35 -0300

    FEAT: adicionado o retorno dos dados requisitados

--- src/app/controllers/inventories.py
+++ src/app/controllers/inventories.py
@@ -6,24 +6,30 @@ from src.app.models.inventory import Inventory, intentories_share_schema
 
 inventory = Blueprint('inventory', __name__, url_prefix='/inventory')
 
-@inventory.route("/", methods = ['GET'])
+@inventory.route("/results", methods = ['GET'])
 def list_all_requirements():
 
-    users_db_data = User.quey.all()
+    users_db_data = User.query.all()
     inventory_db_data = Inventory.query.all()
-
-    users_dict = users_share_schema.dump(users_db_data)
     inventory_dict = intentories_share_schema.dump(inventory_db_data)
 
-    print(users_dict)
-    # print(inventory_dict)
+    total_users = len(users_db_data)
+    total_items = len(inventory_dict)
 
+    total_items_price = 0
+    total_items_loaned = 0
+    for item in inventory_dict:
+        print(item)
+        if item['user_id'] != None:
+            total_items_loaned += 1
+        elif item['value'] > 0 and item['value'] != None:
+            total_items_price += item['value']
 
+    return_dados = {
+        'total_items': total_items, 
+        'total_users': total_users, 
+        'total_items_loaned': total_items_loaned,
+        'total_items_price': total_items_price
+        }
 
-'''
-- Calcular o número de usuários.
-- Calcular o número de itens.
-- Calcular o valor da soma de todos os itens.
-- Calcular quantos itens estão emprestados para usuários.
-- Retornar às estatísticas, além do Status 200 (OK)
-'''
+    return jsonify(return_dados), 200
