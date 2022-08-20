270bce8af7b3fcffc840085f6a265eb765d136e1
josinaldoandradepereira <101839277+josinaldoandradepereira@users.noreply.github.com>
2022-08-19 22:14:29 -0300

    feature/endpoint6-patch-user-id

--- src/app/controllers/users.py
+++ src/app/controllers/users.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, jsonify, request
-
-from src.app.models.user import User, users_roles_share_schema
-
+from src.app.models.user import User, users_roles_share_schema, users_share_schema
+from src.app  import db as Banco
 user = Blueprint('user', __name__, url_prefix='/user')
 
 @user.route("/", defaults = {"users": 1})
@@ -30,4 +29,58 @@ def list_user_per_page(users):
     list_users_dict = users_roles_share_schema.dump(list_users.items)
 
     return jsonify(list_users_dict), 200
+
+@user.route("/<int:users>", methods = ['PATCH'])
+def atualiza_user(users):
+
+        usuario_objeto = User.query.filter_by(id=users).first()
+        body = request.get_json()
+            
+    
+
+        if body['age'] !='' and usuario_objeto:
+            usuario_objeto.age = body['age']
+        if body['city_id'] !='' and usuario_objeto:
+            usuario_objeto.city_id = body['city_id']
+        if body['complement'] !='' and usuario_objeto:
+            usuario_objeto.complement = body['complement']
+        if body['district'] !='' and usuario_objeto:
+            usuario_objeto.district = body['district']
+        if body['email'] !='' and usuario_objeto:
+            usuario_objeto.email = body['email']
+        if body['gender_id'] !='' and usuario_objeto:
+            usuario_objeto.gender_id = body['gender_id']
+        if body['landmark'] !='' and usuario_objeto:
+            usuario_objeto.landmark = body['landmark']                
+        if  body['name'] !='' and usuario_objeto:
+            usuario_objeto.name = body['name']
+        if  body['number_street'] !='' and usuario_objeto:
+            usuario_objeto.number_street = body['number_street']
+        if  body['street'] !='' and usuario_objeto:
+            usuario_objeto.street = body['street']
+        if  body['cep'] !='' and usuario_objeto:
+            usuario_objeto.cep = body['cep']
+        if  body['number_street'] !='' and usuario_objeto:
+            usuario_objeto.number_street = body['number_street']
+        if  body['password'] !='' and usuario_objeto:
+            usuario_objeto.password = User.encrypt_password(body['password'].encode("utf-8"))
+        if  body['phone'] !='' and usuario_objeto:
+            usuario_objeto.phone = body['phone']
+        if  body['role_id'] !='' and usuario_objeto:
+            usuario_objeto.role_id = body['role_id']
+        
+
+        new_user = User(body['gender_id'],body['role_id'],body['city_id'],body['name'],body['age'],body['email'],body['password'],body['phone'],body['cep'],body['street'],body['number_street'],body['complement'],body['landmark'],body['district'])
+        print(new_user)
+            
+      
+        if usuario_objeto:    
+           Banco.session.add(usuario_objeto) 
+           Banco.session.commit()
+           return jsonify('Sucesso'), 204
+
+        
+        return "Não encontrado", 404
+    
+
 
